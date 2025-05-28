96604cdc2e336cd0b5367a21ca687de9f115c763
kosuke55 <kosuke.tnp@gmail.com>
2025-05-28 17:46:37 +0900

    fix unit test
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_stop_line_module/test/test_scene.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_stop_line_module/test/test_scene.cpp
@@ -43,6 +43,7 @@ autoware_internal_planning_msgs::msg::PathPointWithLaneId make_path_point(
 {
   autoware_internal_planning_msgs::msg::PathPointWithLaneId point;
   point.point.pose.position = make_geom_point(x, y);
+  point.lane_ids = {0};
   return point;
 }
 }  // namespace
@@ -89,7 +90,7 @@ protected:
 
     module_ = std::make_shared<StopLineModule>(
       1, stop_line_, 0, planner_param_, rclcpp::get_logger("test_logger"), clock_,
-      std::make_shared<autoware_utils::TimeKeeper>(),
+      std::make_shared<autoware_utils_debug::TimeKeeper>(),
       std::make_shared<autoware::planning_factor_interface::PlanningFactorInterface>(
         node_.get(), "test_stopline"));
 
@@ -131,6 +132,7 @@ TEST_F(StopLineModuleTest, TestGetEgoAndStopPoint)
     const auto [ego_s, stop_point_s] =
       module_->getEgoAndStopPoint(trajectory_, path_, ego_pose, StopLineModule::State::STOPPED);
 
+    EXPECT_TRUE(stop_point_s.has_value());
     EXPECT_DOUBLE_EQ(ego_s, 5.0);
     EXPECT_DOUBLE_EQ(stop_point_s.value(), 5.0);
   }
