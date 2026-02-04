b2cbada688a8dfeed033f18fb8578da56df4764a
Koichi Imai <koichi.imai.2@tier4.jp>
2026-02-04 11:11:44 +0900

    apply agnocast
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- common/autoware_vehicle_info_utils/include/autoware/vehicle_info_utils/vehicle_info_utils.hpp
+++ common/autoware_vehicle_info_utils/include/autoware/vehicle_info_utils/vehicle_info_utils.hpp
@@ -19,25 +19,67 @@
 
 #include <rclcpp/node.hpp>
 
+#include <string>
+
 namespace autoware::vehicle_info_utils
 {
-/// This is a convenience class for saving you from declaring all parameters
-/// manually and calculating derived parameters.
-/// This class supposes that necessary parameters are set when the node is launched.
-class VehicleInfoUtils
+
+namespace detail
+{
+/// Helper function template to get parameter from node
+template <typename T, typename NodeT>
+T getParameter(NodeT & node, const std::string & name)
+{
+  if (node.has_parameter(name)) {
+    return node.get_parameter(name).template get_value<T>();
+  }
+
+  try {
+    return node.template declare_parameter<T>(name);
+  } catch (const rclcpp::ParameterTypeException & ex) {
+    RCLCPP_ERROR(
+      node.get_logger(), "Failed to get parameter `%s`, please set it when you launch the node.",
+      name.c_str());
+    throw;
+  }
+}
+}  // namespace detail
+
+/// Template class for vehicle info utilities
+/// \tparam NodeT The node type (rclcpp::Node or agnocast::Node)
+template <typename NodeT>
+class VehicleInfoUtilsTemplate
 {
 public:
   /// Constructor
-  // NOTE(soblin): this throws which should be replaced with a factory
-  explicit VehicleInfoUtils(rclcpp::Node & node);
+  explicit VehicleInfoUtilsTemplate(NodeT & node)
+  {
+    const auto wheel_radius_m = detail::getParameter<double>(node, "wheel_radius");
+    const auto wheel_width_m = detail::getParameter<double>(node, "wheel_width");
+    const auto wheel_base_m = detail::getParameter<double>(node, "wheel_base");
+    const auto wheel_tread_m = detail::getParameter<double>(node, "wheel_tread");
+    const auto front_overhang_m = detail::getParameter<double>(node, "front_overhang");
+    const auto rear_overhang_m = detail::getParameter<double>(node, "rear_overhang");
+    const auto left_overhang_m = detail::getParameter<double>(node, "left_overhang");
+    const auto right_overhang_m = detail::getParameter<double>(node, "right_overhang");
+    const auto vehicle_height_m = detail::getParameter<double>(node, "vehicle_height");
+    const auto max_steer_angle_rad = detail::getParameter<double>(node, "max_steer_angle");
+
+    vehicle_info_ = createVehicleInfo(
+      wheel_radius_m, wheel_width_m, wheel_base_m, wheel_tread_m, front_overhang_m, rear_overhang_m,
+      left_overhang_m, right_overhang_m, vehicle_height_m, max_steer_angle_rad);
+  }
 
   /// Get vehicle info
-  [[nodiscard]] VehicleInfo getVehicleInfo() const;
+  [[nodiscard]] VehicleInfo getVehicleInfo() const { return vehicle_info_; }
 
 private:
   /// Buffer for base parameters
   VehicleInfo vehicle_info_;
 };
+
+/// Backward compatibility: VehicleInfoUtils is the rclcpp::Node version
+using VehicleInfoUtils = VehicleInfoUtilsTemplate<rclcpp::Node>;
 
 }  // namespace autoware::vehicle_info_utils
 
