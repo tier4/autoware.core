b73a6563450920edef8db35f82ae56fd4514dcbe
Koichi Imai <koichi.imai.2@tier4.jp>
2026-04-14 21:20:16 +0900

    templatize ndt_scan_matcher hyperparameters
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- localization/autoware_ndt_scan_matcher/include/autoware/ndt_scan_matcher/hyper_parameters.hpp
+++ localization/autoware_ndt_scan_matcher/include/autoware/ndt_scan_matcher/hyper_parameters.hpp
@@ -107,70 +107,71 @@ struct HyperParameters
   } dynamic_map_loading{};
 
 public:
-  explicit HyperParameters(agnocast::Node * node)
+  template <typename NodeT>
+  explicit HyperParameters(NodeT * node)
   {
-    frame.base_frame = node->declare_parameter<std::string>("frame.base_frame");
-    frame.ndt_base_frame = node->declare_parameter<std::string>("frame.ndt_base_frame");
-    frame.map_frame = node->declare_parameter<std::string>("frame.map_frame");
+    frame.base_frame = node->template declare_parameter<std::string>("frame.base_frame");
+    frame.ndt_base_frame = node->template declare_parameter<std::string>("frame.ndt_base_frame");
+    frame.map_frame = node->template declare_parameter<std::string>("frame.map_frame");
 
-    sensor_points.timeout_sec = node->declare_parameter<double>("sensor_points.timeout_sec");
+    sensor_points.timeout_sec = node->template declare_parameter<double>("sensor_points.timeout_sec");
     sensor_points.required_distance =
-      node->declare_parameter<double>("sensor_points.required_distance");
+      node->template declare_parameter<double>("sensor_points.required_distance");
 
-    ndt.trans_epsilon = node->declare_parameter<double>("ndt.trans_epsilon");
-    ndt.step_size = node->declare_parameter<double>("ndt.step_size");
-    ndt.resolution = node->declare_parameter<float>("ndt.resolution");
-    ndt.max_iterations = static_cast<int>(node->declare_parameter<int64_t>("ndt.max_iterations"));
-    ndt.num_threads = static_cast<int>(node->declare_parameter<int64_t>("ndt.num_threads"));
+    ndt.trans_epsilon = node->template declare_parameter<double>("ndt.trans_epsilon");
+    ndt.step_size = node->template declare_parameter<double>("ndt.step_size");
+    ndt.resolution = node->template declare_parameter<float>("ndt.resolution");
+    ndt.max_iterations = static_cast<int>(node->template declare_parameter<int64_t>("ndt.max_iterations"));
+    ndt.num_threads = static_cast<int>(node->template declare_parameter<int64_t>("ndt.num_threads"));
     ndt.num_threads = std::max(ndt.num_threads, 1);
-    ndt_regularization_enable = node->declare_parameter<bool>("ndt.regularization.enable");
+    ndt_regularization_enable = node->template declare_parameter<bool>("ndt.regularization.enable");
     ndt.regularization_scale_factor =
-      static_cast<float>(node->declare_parameter<float>("ndt.regularization.scale_factor"));
+      static_cast<float>(node->template declare_parameter<float>("ndt.regularization.scale_factor"));
 
     initial_pose_estimation.particles_num =
-      node->declare_parameter<int64_t>("initial_pose_estimation.particles_num");
+      node->template declare_parameter<int64_t>("initial_pose_estimation.particles_num");
     initial_pose_estimation.n_startup_trials =
-      node->declare_parameter<int64_t>("initial_pose_estimation.n_startup_trials");
+      node->template declare_parameter<int64_t>("initial_pose_estimation.n_startup_trials");
 
     validation.initial_pose_timeout_sec =
-      node->declare_parameter<double>("validation.initial_pose_timeout_sec");
+      node->template declare_parameter<double>("validation.initial_pose_timeout_sec");
     validation.initial_pose_distance_tolerance_m =
-      node->declare_parameter<double>("validation.initial_pose_distance_tolerance_m");
+      node->template declare_parameter<double>("validation.initial_pose_distance_tolerance_m");
     validation.initial_to_result_distance_tolerance_m =
-      node->declare_parameter<double>("validation.initial_to_result_distance_tolerance_m");
+      node->template declare_parameter<double>("validation.initial_to_result_distance_tolerance_m");
     validation.critical_upper_bound_exe_time_ms =
-      node->declare_parameter<double>("validation.critical_upper_bound_exe_time_ms");
+      node->template declare_parameter<double>("validation.critical_upper_bound_exe_time_ms");
     validation.skipping_publish_num =
-      node->declare_parameter<int64_t>("validation.skipping_publish_num");
+      node->template declare_parameter<int64_t>("validation.skipping_publish_num");
 
     const int64_t converged_param_type_tmp =
-      node->declare_parameter<int64_t>("score_estimation.converged_param_type");
+      node->template declare_parameter<int64_t>("score_estimation.converged_param_type");
     score_estimation.converged_param_type =
       static_cast<ConvergedParamType>(converged_param_type_tmp);
     score_estimation.converged_param_transform_probability =
-      node->declare_parameter<double>("score_estimation.converged_param_transform_probability");
+      node->template declare_parameter<double>("score_estimation.converged_param_transform_probability");
     score_estimation.converged_param_nearest_voxel_transformation_likelihood =
-      node->declare_parameter<double>(
+      node->template declare_parameter<double>(
         "score_estimation.converged_param_nearest_voxel_transformation_likelihood");
     score_estimation.no_ground_points.enable =
-      node->declare_parameter<bool>("score_estimation.no_ground_points.enable");
-    score_estimation.no_ground_points.z_margin_for_ground_removal = node->declare_parameter<double>(
+      node->template declare_parameter<bool>("score_estimation.no_ground_points.enable");
+    score_estimation.no_ground_points.z_margin_for_ground_removal = node->template declare_parameter<double>(
       "score_estimation.no_ground_points.z_margin_for_ground_removal");
 
     std::vector<double> output_pose_covariance =
-      node->declare_parameter<std::vector<double>>("covariance.output_pose_covariance");
+      node->template declare_parameter<std::vector<double>>("covariance.output_pose_covariance");
     for (std::size_t i = 0; i < output_pose_covariance.size(); ++i) {
       covariance.output_pose_covariance[i] = output_pose_covariance[i];
     }
-    const int64_t covariance_estimation_type_tmp = node->declare_parameter<int64_t>(
+    const int64_t covariance_estimation_type_tmp = node->template declare_parameter<int64_t>(
       "covariance.covariance_estimation.covariance_estimation_type");
     covariance.covariance_estimation.covariance_estimation_type =
       static_cast<CovarianceEstimationType>(covariance_estimation_type_tmp);
     covariance.covariance_estimation.initial_pose_offset_model_x =
-      node->declare_parameter<std::vector<double>>(
+      node->template declare_parameter<std::vector<double>>(
         "covariance.covariance_estimation.initial_pose_offset_model_x");
     covariance.covariance_estimation.initial_pose_offset_model_y =
-      node->declare_parameter<std::vector<double>>(
+      node->template declare_parameter<std::vector<double>>(
         "covariance.covariance_estimation.initial_pose_offset_model_y");
     if (
       covariance.covariance_estimation.initial_pose_offset_model_x.size() !=
@@ -182,16 +183,16 @@ public:
       throw std::runtime_error(message.str());
     }
     covariance.covariance_estimation.temperature =
-      node->declare_parameter<double>("covariance.covariance_estimation.temperature");
+      node->template declare_parameter<double>("covariance.covariance_estimation.temperature");
     covariance.covariance_estimation.scale_factor =
-      node->declare_parameter<double>("covariance.covariance_estimation.scale_factor");
+      node->template declare_parameter<double>("covariance.covariance_estimation.scale_factor");
 
     dynamic_map_loading.update_distance =
-      node->declare_parameter<double>("dynamic_map_loading.update_distance");
+      node->template declare_parameter<double>("dynamic_map_loading.update_distance");
     dynamic_map_loading.map_radius =
-      node->declare_parameter<double>("dynamic_map_loading.map_radius");
+      node->template declare_parameter<double>("dynamic_map_loading.map_radius");
     dynamic_map_loading.lidar_radius =
-      node->declare_parameter<double>("dynamic_map_loading.lidar_radius");
+      node->template declare_parameter<double>("dynamic_map_loading.lidar_radius");
   }
 };
 
