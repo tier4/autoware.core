68a4fb589fa1ac18abef678d991ca3eb72475583
emmeyteja <emmeyteja@gmail.com>
2026-06-01 12:37:41 +0900

    feat: add visualization support for area primitives (part of reverse maneuver feature) (#1122)
    feat: add visualization for area primitives; part of reverse maneuver feature
    
    Signed-off-by: emmeyteja <emmeyteja@gmail.com>

--- map/autoware_lanelet2_map_visualizer/src/lanelet2_map_visualization_node.cpp
+++ map/autoware_lanelet2_map_visualizer/src/lanelet2_map_visualization_node.cpp
@@ -136,6 +136,12 @@ void Lanelet2MapVisualizationNode::on_map_bin(
     lanelet::utils::query::busStopAreas(all_lanelets);
   lanelet::ConstLineStrings3d waypoints = lanelet::utils::query::getAllWaypoints(viz_lanelet_map);
 
+  std::vector<lanelet::ConstArea> lanelet_areas;
+  lanelet_areas.reserve(viz_lanelet_map->areaLayer.size());
+  for (const auto & area : viz_lanelet_map->areaLayer) {
+    lanelet_areas.push_back(area);
+  }
+
   std_msgs::msg::ColorRGBA cl_road;
   std_msgs::msg::ColorRGBA cl_shoulder;
   std_msgs::msg::ColorRGBA cl_cross;
@@ -164,6 +170,9 @@ void Lanelet2MapVisualizationNode::on_map_bin(
   std_msgs::msg::ColorRGBA cl_bus_stop_area;
   std_msgs::msg::ColorRGBA cl_bicycle_lane;
   std_msgs::msg::ColorRGBA cl_waypoints;
+  std_msgs::msg::ColorRGBA cl_lanelet_routing_area;
+  std_msgs::msg::ColorRGBA cl_lanelet_routing_area_outline;
+
   set_color(&cl_road, 0.27, 0.27, 0.27, 0.999);
   set_color(&cl_shoulder, 0.15, 0.15, 0.15, 0.999);
   set_color(&cl_cross, 0.27, 0.3, 0.27, 0.5);
@@ -192,6 +201,10 @@ void Lanelet2MapVisualizationNode::on_map_bin(
   set_color(&cl_bus_stop_area, 0.863, 0.863, 0.863, 0.5);
   set_color(&cl_bicycle_lane, 0.0, 0.3843, 0.6274, 0.5);
   set_color(&cl_waypoints, 0.6, 0.4, 0.3, 0.999);
+  // Static map fill for areaLayer namespace lanelet_routing_area; darker edge in
+  // lanelet_routing_area_outline
+  set_color(&cl_lanelet_routing_area, 0.8, 0.53, 0.25, 0.35);
+  set_color(&cl_lanelet_routing_area_outline, 0.45, 0.28, 0.12, 0.95);
 
   visualization_msgs::msg::MarkerArray map_marker_array;
 
@@ -323,6 +336,9 @@ void Lanelet2MapVisualizationNode::on_map_bin(
   insert_marker_array(
     &map_marker_array,
     lanelet::visualization::lineStringsAsMarkerArray(waypoints, "waypoints", cl_waypoints, 0.02));
+  insert_marker_array(
+    &map_marker_array, lanelet::visualization::laneletAreasAsMarkerArray(
+                         lanelet_areas, cl_lanelet_routing_area, cl_lanelet_routing_area_outline));
 
   pub_marker_->publish(map_marker_array);
 }
