5e7063e0219536092d90ac6fd1b665b4cb28836a
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2026-06-19 17:34:24 +0900

    fix: use isolated test
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/autoware_planning_factor_interface/test/test_planning_factor_interface.cpp
+++ planning/autoware_planning_factor_interface/test/test_planning_factor_interface.cpp
@@ -174,6 +174,9 @@ class PlanningFactorInterfaceTest : public ::testing::Test
 protected:
   void SetUp() override
   {
+    if (!rclcpp::ok()) {
+      rclcpp::init(0, nullptr);
+    }
     node_ = std::make_shared<rclcpp::Node>("test_node");
     interface_ = std::make_unique<PlanningFactorInterface>(node_.get(), "test");
   }
@@ -242,11 +245,3 @@ TEST_F(PlanningFactorInterfaceTest, PublishExternalBuilderDoesNotAffectInternal)
   EXPECT_EQ(interface_->get_factors().size(), 1u);
 }
 
-int main(int argc, char ** argv)
-{
-  testing::InitGoogleTest(&argc, argv);
-  rclcpp::init(argc, argv);
-  const int result = RUN_ALL_TESTS();
-  rclcpp::shutdown();
-  return result;
-}
